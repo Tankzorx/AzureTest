6e5969826f09dab973f38d2e6911cc70a31d07f3
Tankzorx <gustavj.dotzorx@gmail.com>
2017-11-09 21:21:17 +0100

    Set up typescript

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import * as express from "express";
+
+
+
+
+const app = express()
+
+app.get("/", (req, res) => res.send("hello world!"))
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
