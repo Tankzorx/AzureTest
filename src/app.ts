ce48369b292add6e50fbf9b16dfc0af959aca6b1
Tankzorx <gustavj.dotzorx@gmail.com>
2017-11-09 21:42:00 +0100

    created a generator that yields the natural numbers

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import * as express from "express";
 
-
-
+function* myGenerator () {
+    let i = 1
+    while (true) {
+        yield i
+        i += 1
+    }
+}
 
 const app = express()
 
-app.get("/", (req, res) => res.send("hello world!"))
+var myIterator = myGenerator()
+
+app.get("/", (req, res) => {
+    res.send(`hello world! Number: ${myIterator.next().value} `)
+})
 
 const port = process.env.PORT || 3000
 
